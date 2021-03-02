e9728e1721c0a69ede3b99d2b1640bf6f5ffdb01
RomanNunez <roman_nunez@ymail.com>
2021-03-02 13:20:00 -0600

    Implement a route for api/users

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var apiUsersRouter = require('./routes/api/users');
+
 var app = express();
 var mongoose = require('mongoose');
 
@@ -27,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/api/users', apiUsersRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
