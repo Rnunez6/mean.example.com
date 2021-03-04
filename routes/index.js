dd378f4d2ad61b79209c9ad2e8e7b053fb4b331f
RomanNunez <roman_nunez@ymail.com>
2021-03-04 10:11:18 -0600

    Add logout methods

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Roman'});
+  console.log(req.session);
+  res.render('index', { title: 'Express' });
 });
-
 module.exports = router;
